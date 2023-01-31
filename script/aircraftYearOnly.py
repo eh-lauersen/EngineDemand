19f55ed2badf130af84b6b3712b71409e7fcdbe8
eh-lauersen <ellen.lauersen@toplau.dk>
2023-01-31 14:17:43 +0100

    Debugged aircraftYearOnly and made YearOnly files.

--- script/aircraftYearOnly.py
+++ script/aircraftYearOnly.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import os
 
-def aircraftYearOnly (infile: str, aircraft: str):
+
+def aircraft_year_only(infile: str, aircraft: str):
     
     """
     Will make a CSV-file containing only one column with only the year that an aircraft had its first flight.
-    
+
     Input:
         file: the CSV-file for an aircraft
         aircraft: the type of aircraft
@@ -15,20 +16,23 @@ def aircraftYearOnly (infile: str, aircraft: str):
     """
     
     # creating paths
-    path = os.path.join("data", "cleanData")
+    path = os.path.join("..", "data")
     data = os.path.join(path, infile)
     
     # loading dataset
     df = pd.read_csv(data)
     
     # extracting year
-    yearonly = df["f/f"].str[6:]
+    year_only = df["f/f"].str[6:]
     
     # outfile
     outfile = os.path.join(path, "aircraftYearOnly", f"{aircraft}year.csv")
-    yearonly.to_csv(outfile, index = False)
+    year_only.to_csv(outfile, index=False)
     
     # user output
     print(f"File processed")
     
     return None
+
+
+aircraft_year_only()
